8d8dba1643f13cbbddaddeff3b5b671035667a4e
gravel223 <mazda4697@gmail.com>
2020-09-24 21:49:05 +0400

    update selection

--- lesson_7/selectors.rb
+++ lesson_7/selectors.rb
@@ -124,11 +124,47 @@ module Selectors
     puts "[-] ОШИБКА: #{e.message}"
   end
 
-  def show_stations(stations)
-    stations.each do |station|
+  def show_wagons(train)
+    puts " - #{get_type(train.type)} поезд №#{train.number} кол-во вагонов:#{train.wagons.size}"
+    train.each_wagons do |wagon, i|
+      if wagon.cargo?
+        print " - - вагон № #{i} - #{get_type(wagon.type)},"
+        print " cвободных мест: #{wagon.free_capacity},"
+        puts " занято: #{wagon.used_capacity}"
+      else
+        print " - - вагон №#{i} - #{get_type(wagon.type)},"
+        print " cвободного места : #{wagon.seat_free},"
+        puts " занято: #{wagon.used_seats}"
+      end
+    end
+  end
+
+  def show_stations
+    block = proc { |train| show_wagons train }
+
+    @stations.each do |station|
       puts "[!] станция: #{station.name.capitalize}"
-      station.trains.each { |train| puts "\tпоезд №#{train.number}" unless station.trains.nil? }
+      station.each_trains block
     end
     wait_pressing
+  end
+
+  def show_trains
+    @trains.each { |train| show_wagons train }
+    wait_pressing
+  end
+
+  def create_passenger_wagon
+    print ' укажите количество мест в вагоне: '
+    PassengerWagon.new(gets.to_i)
+  end
+
+  def create_cargo_wagon
+    print 'укажите объем вагона: '
+    CargoWagon.new(gets.to_i)
+  end
+
+  def get_type(type)
+    type.eql?(:cargo) ? 'грузовой' : 'пассажирский'
   end
 end
